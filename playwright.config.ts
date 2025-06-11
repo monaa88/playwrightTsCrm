d477f316eff3f15a6ea23f6bc48c17dac48537d8
monaa88 <bmohanapriya38@gmail.com>
2025-06-11 16:18:19 +0530

    Adding api cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   /* Timeout for the entire test */
   testDir: "./tests",
@@ -40,8 +29,6 @@ export default defineConfig({
     // Wait for elements by default
     // Automatically wait for these states
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
@@ -52,37 +39,5 @@ export default defineConfig({
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
--- tests/ApiTests/demoApi.spec.ts
+++ tests/ApiTests/demoApi.spec.ts
@@ -7,3 +7,25 @@ test("api", async ({ request }) => {
   const data = await response.json();
   console.log(data);
 });
+
+test("api with query params", async ({ request }) => {
+  const response = await request.post(
+    "https://restful-booker.herokuapp.com/booking",
+    {
+      data: {
+        firstname: "jjkk",
+        lastname: "Brown",
+        totalprice: 111,
+        depositpaid: true,
+        bookingdates: {
+          checkin: "2018-01-01",
+          checkout: "2019-01-01",
+        },
+        additionalneeds: "Breakfast",
+      },
+    }
+  );
+
+  const data = await response.json();
+  console.log(data);
+});
